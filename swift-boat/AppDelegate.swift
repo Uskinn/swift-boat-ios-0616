c993b46ecda7ade768e402ed1578ecee60cebcac
Uskinn <s.nevzor@yahoo.com>
2016-07-12 13:27:45 -0400

    Done

--- swift-boat/AppDelegate.swift
+++ swift-boat/AppDelegate.swift
@@ -4,18 +4,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+                // Override point for customization after application launch.
         
         /**
-
-        * Test your custom class here.
-
-        */
+         
+         * Test your custom class here.
+         
+         */
         
         // Do not alter
         return true  //
